8593beda7c82b01e2a45bb82abcc210b612c27e7
Xinyi Shen <shenxinyi@Xinyis-MacBook-Pro.local>
2025-04-12 15:16:02 -0700

    modified api for testing

--- app/clients/api/router.py
+++ app/clients/api/router.py
@@ -38,14 +38,14 @@ async def get_clients(
     return ClientRepository(db).get_clients(skip, limit)
 
 
-@router.get("/{client_id}", response_model=ClientResponse)
-async def get_client(
-    client_id: int,
-    current_user: User = Depends(get_admin_user),
-    db: Session = Depends(get_db),
-):
-    """Get a specific client by ID"""
-    return ClientRepository(db).get_client(client_id)
+# @router.get("/{client_id}", response_model=ClientResponse)
+# async def get_client(
+#     client_id: int,
+#     current_user: User = Depends(get_admin_user),
+#     db: Session = Depends(get_db),
+# ):
+#     """Get a specific client by ID"""
+#     return ClientRepository(db).get_client(client_id)
 
 
 @router.put("/{client_id}", response_model=ClientResponse)
